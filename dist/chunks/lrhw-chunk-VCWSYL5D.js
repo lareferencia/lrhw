6a0c68c0fc980144d40cdf83026bac7ddf670c2a
Lautaro Matas <lmatas@gmail.com>
2023-09-22 13:53:51 +0200

    update

---
+++ dist/chunks/lrhw-chunk-VCWSYL5D.js
@@ -0,0 +1,66 @@
+import{c as i,e as t,g as o}from"./lrhw-chunk-6XPTVWZU.js";t();t();var r="68d847c38ed6359d2f075dafd9c68b723b249d9b638030ecaac9b36ac352ca73",_=`
+._preview_img_18fy5_2 {
+    cursor: pointer;
+  }
+
+._error_container_18fy5_6 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    margin: 0 auto;
+    padding: 2rem;
+    font-family: 'Poppins', sans-serif; 
+    line-height: 1.5;
+    font-weight: 400;
+    color: #1a1a1a;
+  }
+
+._container_18fy5_19 {
+    height: 100%;
+    margin: 0 auto;
+    padding: 2rem;
+    font-family: 'Poppins', sans-serif; 
+    line-height: 1.5;
+    font-weight: 400;
+    border-radius: 10px;
+    color: #1a1a1a;
+  }
+
+  ._loadingContainer_18fy5_30 {
+    height: 450px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  ._timeUnitButton_18fy5_37 {
+    padding: 0.3em 0.8em;
+    border: 1px solid rgba(128, 128, 128, 0.393);
+    border-radius: 5px;
+    font-size: 14px;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: transparent;
+    color: black;
+    cursor: pointer;
+    transition: 0.3s ease-in-out;
+    opacity: 0.5;
+    margin-right: 6px;
+  }
+
+  ._timeUnitButton_18fy5_37:hover {
+    background-color: grey;
+    color: white;
+    transition: 0.3s ease-in-out;
+  }
+
+  ._buttonActive_18fy5_58 {
+    opacity: 1;
+    background-color: grey;
+    color: white;
+  }
+
+
+  
+  `;(function(){if(typeof document!="undefined"&&!document.getElementById(r)){var e=document.createElement("style");e.id=r,e.textContent=_,document.head.appendChild(e)}})();var a={previewImg:"_preview_img_18fy5_2",errorContainer:"_error_container_18fy5_6",container:"_container_18fy5_19",loadingContainer:"_loadingContainer_18fy5_30",timeUnitButton:"_timeUnitButton_18fy5_37",buttonActive:"_buttonActive_18fy5_58"};var n=i(o()),d=()=>(0,n.jsx)("div",{className:a.error_container,children:(0,n.jsx)("h1",{children:"No se encontrar\xF3 informacion"})}),l=d;export{l as default};
